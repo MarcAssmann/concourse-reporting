8583c6c36d10b5681103dcb26193fbc415358620
Marc Aßmann <marc.assmann@gmail.com>
2017-07-24 22:39:39 +0200

    first commit

---
+++ report.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+curl -o fly http://192.168.100.4:8080/api/v1/cli?arch=amd64&platform=linux
+chmod +x fly
+
+fly -t lite login -c http://192.168.100.4:8080
+
+fly -t lite pipelines
+
+fly -t lite builds
+
